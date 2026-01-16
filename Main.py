adc8ca14bb740774e7ab8898adca24c3557a6740
Nyctophobe <simonlopezt@gmail.com>
2026-01-16 13:52:35 -0500

    Implementing movement

--- Main.py
+++ Main.py
@@ -25,17 +25,13 @@ def main():
     basicText = pygame.font.SysFont('Arial', 20)
 
 #____________________________________________________________Main loop____________________________________________________________
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    RUNNING = True
+    while RUNNING:
+#____________________________________________________________Display World____________________________________________________________
+
 
         # Fill the screen with a color (RGB)
         drawSurface.fill((0, 50, 255))
-
-       
-
         """
         #Egg Sprite Sheet
         egg = pygame.image.load(os.path.join("Game Sprites.png")).convert()
@@ -43,17 +39,28 @@ def main():
         egg.set_colorkey((0, 0, 0))
         drawSurface.blit(egg, (200, 100), pygame.Rect(0, 0, 16, 16))
         """
-
         #Game Text
         helloWorld = basicText.render("hello world", False, (255,0,0))
         drawSurface.blit(helloWorld, (0,0))
 
-
+        #Upscale Text
         pygame.transform.scale(drawSurface,
                             list(UPSCALED),
                             screen)
         # Update the display
         pygame.display.flip()
+#____________________________________________________________Event Keys____________________________________________________________
+
+        for event in pygame.event.get():
+            #Quit Event
+            if event.type == pygame.QUIT or \
+               (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                RUNNING = False
+
+            #Other Events
+            #else
+#____________________________________________________________Update Objects____________________________________________________________
+           
 
 
     # Quit Pygame
