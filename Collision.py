9d1982b685f956ac194a5d576214939315dab9e3
Nyctophobe <simonlopezt@gmail.com>
2026-02-02 13:42:56 -0500

    Added standart collision.

---
+++ Collision.py
@@ -0,0 +1,19 @@
+"""
+Allows the game to know when two or more objects are overlaping, allowing a chosen response
+"""
+
+#collisionRect = rectAdd(position, hitBox)
+"""
+rect1.x < rect2.x+rect2.width
+rect1.x > rect2.x+rect2.width
+rect1.y < rect2.y+rect2.height
+rect1.y > rect2.y+rect2.height
+or
+rect1.colliderect(rect2) = new rect size (How much is colliding)
+0,0,16,16
+3, 1, 14, 11
+3 pixels left from top left corner
+1 pixel right from top left corner
+14 pixels tall, 11, pixels wide
+
+"""
